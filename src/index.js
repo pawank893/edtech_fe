0bbea6a308feb802fb45dd1cdd383a22e903781e
Pawan <pawan@headout.com>
2019-03-17 21:06:58 +0530

    production env

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ const routing = (
 
 
 ReactDOM.render(routing, document.getElementById('root'))
+document.title = "Ameltus";
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
